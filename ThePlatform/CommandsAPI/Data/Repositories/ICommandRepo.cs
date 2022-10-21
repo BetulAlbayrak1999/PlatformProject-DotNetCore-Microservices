d44e82ea39342f2f30de8d13b1f69fd5266e4bbe
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-10-21 20:01:31 +0300

    updated

---
+++ ThePlatform/CommandsAPI/Data/Repositories/ICommandRepo.cs
@@ -0,0 +1,20 @@
+﻿using CommandsAPI.Models;
+
+namespace CommandsAPI.Data.Repositories
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlaformExits(int platformId);
+        bool ExternalPlatformExists(int externalPlatformId);
+
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
