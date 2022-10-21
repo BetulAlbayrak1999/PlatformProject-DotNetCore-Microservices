0d868cb59946c1cd5a2abab2575cafc423cdd4d4
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-10-21 19:48:29 +0300

    updated

--- ThePlatform/PlatformAPI/Program.cs
+++ ThePlatform/PlatformAPI/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using PlatformAPI.Data;
 using PlatformAPI.Data.Repositories;
 using PlatformAPI.SyncDataAPIs.Http;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -12,14 +17,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt =>
-                     opt.UseInMemoryDatabase("InMem"));
-builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-var app = builder.Build();
 
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+Console.WriteLine("--> Using InMem Db");
+builder.Services.AddDbContext<AppDbContext>(opt =>
+     opt.UseInMemoryDatabase("InMem"));
+
+/*Console.WriteLine("--> Using SqlServer Db");
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));*/
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
